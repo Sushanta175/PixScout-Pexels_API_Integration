8d06a6efefa7ec4958d08680e1e38729ca92152f
Sushanta Sahu <sushantasahu175@gmail.com>
2024-12-08 10:35:32 +0530

    create models for photos

---
+++ models/photo.go
@@ -0,0 +1,31 @@
+package models
+
+type PhotoSearchResult struct {
+	Page         int32   `json:"page"`
+	PerPage      int32   `json:"per_page"`
+	TotalResults int32   `json:"total_Results"`
+	NextPage     string  `json:"next_page"`
+	PrevPage     string  `json:"prev_page"`
+	Photos       []Photo `json:"photos"`
+}
+
+type Photo struct {
+	Id              int32       `json:"id"`
+	Width           int32       `json:"width"`
+	Height          int32       `json:"height"`
+	Url             string      `json:"url"`
+	Photographer    string      `json:"photographer"`
+	PhotographerUrl string      `json:"photographer_url"`
+	Src             PhotoSource `json:"src"`
+}
+
+type PhotoSource struct {
+	Original  string `json:"original"`
+	Large     string `json:"large"`
+	Large2x   string `json:"large2x"`
+	Medium    string `json:"medium"`
+	Small     string `json:"small"`
+	Portrait  string `json:"portrait"`
+	LandScape string `json:"landscape"`
+	Tiny      string `json:"tiny"`
+}
