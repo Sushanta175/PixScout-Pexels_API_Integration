5e9193f797cc72dd4371ce0b662ba65b59e1179e
Sushanta Sahu <sushantasahu175@gmail.com>
2024-12-08 20:18:02 +0530

    fix json tags in PhotoSearchResult

--- models/photo.go
+++ models/photo.go
@@ -3,7 +3,7 @@ package models
 type PhotoSearchResult struct {
 	Page         int32   `json:"page"`
 	PerPage      int32   `json:"per_page"`
-	TotalResults int32   `json:"total_Results"`
+	TotalResults int32   `json:"total_results"`
 	NextPage     string  `json:"next_page"`
 	PrevPage     string  `json:"prev_page"`
 	Photos       []Photo `json:"photos"`
