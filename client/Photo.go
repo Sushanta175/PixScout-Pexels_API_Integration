d140e88d4111affaf7af7f4605608c127b0da740
Sushanta Sahu <sushantasahu175@gmail.com>
2024-12-08 17:25:51 +0530

    add GetPhoto and GetRandomPhoto function

--- client/Photo.go
+++ client/Photo.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"math/rand"
 
 	"github.com/Sushanta175/Go_Pexels_API/config"
 	"github.com/Sushanta175/Go_Pexels_API/models"
@@ -71,4 +72,47 @@ func (c *Client) CuratedPhotos(per_page, page int) (*models.CuratedPhotosResult,
 	}
 
 	return &result, nil
+}
+
+func (c *Client) GetPhoto(id int32) (*models.Photo, error) {
+	url := fmt.Sprintf(config.PhotoApi+"photos/%d", id)
+
+	resp, err := c.RequestDoWithAuth("GET", url)
+	if resp != nil {
+		return nil, fmt.Errorf("error sending request: %w", err)
+	}
+
+	defer func() {
+		if resp.Body != nil {
+			resp.Body.Close()
+		}
+	}()
+
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return nil, fmt.Errorf("api error: received status code %d", resp.StatusCode)
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response body: %w", err)
+	}
+
+	var result models.Photo
+	err = json.Unmarshal(data, &result)
+	if err != nil {
+		return nil, fmt.Errorf("error Unmarshalling JSON: %w", err)
+	}
+
+	return &result, nil
+}
+
+func (c *Client) GetRandomPhoto() (*models.Photo, error) {
+	page := rand.Intn(1001)
+
+	result, err := c.CuratedPhotos(1, page)
+	if err == nil && len(result.Photos) == 1 {
+		return &result.Photos[0], nil
+	}
+
+	return nil, err
 }
