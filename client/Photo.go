d758af737efcf64bbd87c1d8b375a69fa6db8c25
Sushanta Sahu <sushantasahu175@gmail.com>
2024-12-08 17:54:59 +0530

    refactor the code by creating two new functions performRequestWithAuth and parseResponseBody, hence making the code simpler

--- client/Photo.go
+++ client/Photo.go
@@ -5,105 +5,79 @@ import (
 	"fmt"
 	"io"
 	"math/rand"
+	"net/http"
 
 	"github.com/Sushanta175/Go_Pexels_API/config"
 	"github.com/Sushanta175/Go_Pexels_API/models"
 )
 
-func (c *Client) SearchPhotos(query string, perPage, page int) (*models.PhotoSearchResult, error) {
-	url := fmt.Sprintf(config.PhotoApi+"search?query=%s&per_page=%d&page=%d", query, perPage, page)
-
-	resp, err := c.RequestDoWithAuth("GET", url)
+func (c *Client) performRequestWithAuth(method, url string) (*http.Response, error) {
+	resp, err := c.RequestDoWithAuth(method, url)
 	if err != nil {
 		return nil, fmt.Errorf("error making request: %v", err)
 	}
 
-	defer func() {
-		if resp.Body != nil {
-			defer resp.Body.Close()
-		}
-	}()
-
 	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		defer resp.Body.Close()
 		return nil, fmt.Errorf("api error: received status code %d", resp.StatusCode)
 	}
 
+	return resp, nil
+}
+
+func parseResponseBody[T any](resp *http.Response, result *T) error {
+	defer resp.Body.Close()
+
 	data, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, fmt.Errorf("error reading response body: %v", err)
+		return fmt.Errorf("error reading response body: %v", err)
+	}
+
+	err = json.Unmarshal(data, result)
+	if err != nil {
+		return fmt.Errorf("error Unmarshalling JSON: %v", err)
+	}
+
+	return nil
+}
+
+func (c *Client) SearchPhotos(query string, perPage, page int) (*models.PhotoSearchResult, error) {
+	url := fmt.Sprintf(config.PhotoApi+"search?query=%s&per_page=%d&page=%d", query, perPage, page)
+
+	resp, err := c.performRequestWithAuth("GET", url)
+	if err != nil {
+		return nil, fmt.Errorf("error making request: %v", err)
 	}
 
 	var result models.PhotoSearchResult
-	err = json.Unmarshal(data, &result)
-	if err != nil {
-		return nil, fmt.Errorf("error Unmarshalling JSON: %v", err)
-	}
-
-	return &result, nil
+	err = parseResponseBody(resp, &result)
+	return &result, err
 }
 
 func (c *Client) CuratedPhotos(per_page, page int) (*models.CuratedPhotosResult, error) {
 	url := fmt.Sprintf(config.PhotoApi+"curated?per_page=%d&page=%d", per_page, page)
 
-	resp, err := c.RequestDoWithAuth("GET", url)
+	resp, err := c.performRequestWithAuth("GET", url)
 	if err != nil {
 		return nil, fmt.Errorf("error making request: %w", err)
 	}
 
-	defer func() {
-		if resp.Body != nil {
-			resp.Body.Close()
-		}
-	}()
-
-	if resp.StatusCode < 200 || resp.StatusCode > 299 {
-		return nil, fmt.Errorf("api error: received status code %d", resp.StatusCode)
-	}
-
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("error reading response body: %w", err)
-	}
-
 	var result models.CuratedPhotosResult
-	err = json.Unmarshal(data, &result)
-	if err != nil {
-		return nil, fmt.Errorf("error Unmarshalling JSON: %w", err)
-	}
-
-	return &result, nil
+	err = parseResponseBody(resp, &result)
+	return &result, err
 }
 
 func (c *Client) GetPhoto(id int32) (*models.Photo, error) {
 	url := fmt.Sprintf(config.PhotoApi+"photos/%d", id)
 
-	resp, err := c.RequestDoWithAuth("GET", url)
+	resp, err := c.performRequestWithAuth("GET", url)
 	if resp != nil {
 		return nil, fmt.Errorf("error sending request: %w", err)
 	}
 
-	defer func() {
-		if resp.Body != nil {
-			resp.Body.Close()
-		}
-	}()
-
-	if resp.StatusCode < 200 || resp.StatusCode > 299 {
-		return nil, fmt.Errorf("api error: received status code %d", resp.StatusCode)
-	}
-
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("error reading response body: %w", err)
-	}
-
 	var result models.Photo
-	err = json.Unmarshal(data, &result)
-	if err != nil {
-		return nil, fmt.Errorf("error Unmarshalling JSON: %w", err)
-	}
-
-	return &result, nil
+	err = parseResponseBody(resp, &result)
+	return &result, err
 }
 
 func (c *Client) GetRandomPhoto() (*models.Photo, error) {
