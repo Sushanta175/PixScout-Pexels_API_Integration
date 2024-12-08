a105aa85d40bab5c7ca70b9032c8328f6a86a27b
Sushanta Sahu <sushantasahu175@gmail.com>
2024-12-08 19:30:43 +0530

    make changes to main.go file

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"fmt"
 
+	"github.com/Sushanta175/Go_Pexels_API/client"
 	"github.com/Sushanta175/Go_Pexels_API/config"
 )
 
 func main() {
 	cfg := config.LoadConfig()
 
-	var c = NewClient(cfg.ApiToken)
+	var c = client.NewClient(cfg.ApiToken)
 
 	result, err := c.SearchPhotos("waves")
 	if err != nil {
