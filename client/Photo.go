bdef48b99e63778a9856fa0bd3081906b243a73c
Sushanta Sahu <sushantasahu175@gmail.com>
2024-12-08 14:54:42 +0530

    add CuratedPhotos function

--- client/Photo.go
+++ client/Photo.go
@@ -40,3 +40,35 @@ func (c *Client) SearchPhotos(query string, perPage, page int) (*models.PhotoSea
 
 	return &result, nil
 }
+
+func (c *Client) CuratedPhotos(per_page, page int) (*models.CuratedPhotosResult, error) {
+	url := fmt.Sprintf(config.PhotoApi+"curated?per_page=%d&page=%d", per_page, page)
+
+	resp, err := c.RequestDoWithAuth("GET", url)
+	if err != nil {
+		return nil, fmt.Errorf("error making request: %w", err)
+	}
+
+	defer func() {
+		if resp.Body != nil {
+			resp.Body.Close()
+		}
+	}()
+
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return nil, fmt.Errorf("api error: received status code %w", resp.StatusCode)
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response body: %w", err)
+	}
+
+	var result models.CuratedPhotosResult
+	err = json.Unmarshal(data, &result)
+	if err != nil {
+		return nil, fmt.Errorf("error Unmarshalling JSON: %w", err)
+	}
+
+	return &result, nil
+}
