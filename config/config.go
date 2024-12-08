3ffe36bbe8608e35be4a95eed436256f4dc375d6
Sushanta Sahu <sushantasahu175@gmail.com>
2024-12-08 09:28:00 +0530

    create load config function to load the api token

---
+++ config/config.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"log"
+	"os"
+)
+
+type Config struct {
+	ApiToken string
+}
+
+func loadConfig() *Config {
+	apiToken := os.Getenv("Pexels_Token")
+	if apiToken == "" {
+		log.Fatal("Pexels API Token is not set in the enviorment variables.")
+	}
+
+	return &Config{
+		ApiToken: apiToken,
+	}
+}
