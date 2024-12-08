736a88c2a08cf0495ad82a79fba8eb5a8cd2caf9
Sushanta Sahu <sushantasahu175@gmail.com>
2024-12-08 20:02:12 +0530

    add gotdotenv library and load the .env file and also add log library to handle errors in a better way

--- main.go
+++ main.go
@@ -2,22 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/Sushanta175/Go_Pexels_API/client"
 	"github.com/Sushanta175/Go_Pexels_API/config"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+
 	cfg := config.LoadConfig()
 
 	var c = client.NewClient(cfg.ApiToken)
 
 	result, err := c.SearchPhotos("waves", 15, 1)
 	if err != nil {
-		fmt.Errorf("Search Error %v", err)
+		log.Printf("Search Error %v", err)
 	}
 	if result.Page == 0 {
-		fmt.Errorf("Something wrong in the search")
+		log.Printf("Something wrong in the search")
 	}
 	fmt.Println(result)
 }
