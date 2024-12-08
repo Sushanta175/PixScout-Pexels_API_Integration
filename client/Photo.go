f0e45884d6b0901768c6c977b81a956fe5404005
Sushanta Sahu <sushantasahu175@gmail.com>
2024-12-08 12:46:00 +0530

    make some changes to photo.go

--- client/Photo.go
+++ client/Photo.go
@@ -10,7 +10,7 @@ import (
 )
 
 func (c *Client) SearchPhotos(query string, perPage, page int) (*models.PhotoSearchResult, error) {
-	url := fmt.Sprintf(config.PhotoApi+"search?query=%s&per_page=%s&page=%s", query, perPage, page)
+	url := fmt.Sprintf(config.PhotoApi+"search?query=%s&per_page=%d&page=%d", query, perPage, page)
 
 	resp, err := c.RequestDoWithAuth("GET", url)
 	if err != nil {
@@ -24,18 +24,18 @@ func (c *Client) SearchPhotos(query string, perPage, page int) (*models.PhotoSea
 	}()
 
 	if resp.StatusCode < 200 || resp.StatusCode > 299 {
-		return nil, fmt.Errorf("API error: received status code %d", resp.StatusCode)
+		return nil, fmt.Errorf("api error: received status code %d", resp.StatusCode)
 	}
 
 	data, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, fmt.Errorf("Error reading response body: %v", err)
+		return nil, fmt.Errorf("error reading response body: %v", err)
 	}
 
 	var result models.PhotoSearchResult
 	err = json.Unmarshal(data, &result)
 	if err != nil {
-		return nil, fmt.Errorf("Error Unmarshalling JSON: %v", err)
+		return nil, fmt.Errorf("error Unmarshalling JSON: %v", err)
 	}
 
 	return &result, nil
